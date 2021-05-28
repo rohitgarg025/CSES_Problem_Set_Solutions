c1d0010cb5447c835e18783114256fa58e8e44b5
rohigarg025 <rohitgarg025@gmail.com>
2021-05-29 01:10:49 +0530

    factory_machine _pq implementation done

--- factory_machines.cpp
+++ factory_machines.cpp
@@ -10,104 +10,54 @@
 
 using namespace std;
 void solve(){
-    int n;
-    cin >> n;
-    // [a,b,c] -> a stores arrival time, b stores dept time, c stores index
-    vector <pair <pair <int,int>,int> > customers(n);
-    for(int i=0;i<n;i++){
-        cin >> customers[i].first.first >> customers[i].first.second;
-        customers[i].second = i;
+    // input
 
+    // priority queue implementation 
+    int n,t;
+    cin >> n >> t;
+    vector <int> v(n);
+    // min heap of pairs
+    priority_queue<pair<int,int>, vector<pair <int,int> >, greater<pair<int,int> > > pq;
+
+    for(int i=0;i<n;i++){
+        int x; 
+        cin >> x;
+        v[i] = x;
+        pq.push({x,i});
     }
 
-    // sort them based on arrival time
-    sort(customers.begin(), customers.end());
+    int count = 0;
 
-    //room assigned to each customer
-    vector <int> ans(n,-1);
-
-    int roomid = 1;
-
-    // make a priority queue with [departure times, room occupied]
-    priority_queue <pair<int,int>, vector<pair <int,int> >, greater<pair<int,int> >  > rooms;
-
-    for (int i = 0; i < n; i++)
-    {
-        if(rooms.size() == 0){
-            // if queue is empty
-            // allot this man last room
-            ans[customers[i].second] = roomid++;
-            // fill this entry in the queue
-            rooms.push(make_pair(customers[i].first.second, roomid-1));
-
-         }
-         else{
-            //  if queue is not empty
-             if(rooms.top().first < customers[i].first.first){
-                //  if arrival time of guest is later than departure time of the resident then his room can be alloted to the guest
-                 ans[customers[i].second] = rooms.top().second;
-                 pair <int,int> x = rooms.top();
-                 rooms.pop();
-                 rooms.push(make_pair(customers[i].first.second, x.second));
-
-             }
-             else{
-                //  else he will be allocated a new room 
-                 ans[customers[i].second] = roomid++;
-                 rooms.push(make_pair(customers[i].first.second, roomid-1));
-             }
-         }         
+    while(!pq.empty()){
+        pair <int,int> x = pq.top();
+        pq.pop();
+        count++;
+        if(count == t){cout << x.first << endl; return ;}
+        pq.push({x.first + v[x.second],x.second});
     }
     
+    
 
-    cout << roomid-1 << endl;
-
-    for(int i=0;i<n;i++){
-       cout << ans[i] << " ";
-    }
-
-    cout << endl;
+    
 
 
+    // M2 (0-2) M1(0-3) M3(0-5) M2(2-4) M1(3-6) M2(4-6) M2(6-8)
+    // Input:
+    // 3 7
+    // 3 2 5
+
+    // Output:
+    // 8
+
+    // Explanation: Machine 1 makes two products, machine 2 makes four products and machine 3 makes one product.
+
+
+    
 }
 
 int main()
 {
     FAST_IO;
-    int n;
-    cin >>n;
-    vector <pair <int,int> > in(n), out(n);
-
-    set <int> free;
-
-    for(int i=0;i<n;i++){
-        cin >> in[i].first;
-        cin >> out[i].first;
-        in[i].second = out[i].second = i;
-        free.insert(i);
-    }
-
-    sort(in.begin(), in.end());
-    sort(out.begin(), out.end());
-    
-    vector <int> ans(n);
-
-    for(int i=0,j=0;i<n;i++){
-        while(j<n && out[j].first < in[i].first){
-            free.insert(ans[out[j].second]);
-            j++;
-        }
-
-        ans[in[i].second] = *free.begin();
-        free.erase(free.begin());
-    }
-
-    int k = *max_element(ans.begin(), ans.end()) + 1;
-    cout << k << endl;
-
-    for(int i=0;i<n;i++){
-        cout << ans[i] + 1 << " ";
-    }
-
+    solve();
     return 0;
 }
